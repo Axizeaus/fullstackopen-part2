9a34ba2d5bb90ecdb37c82c5f2840de8ca4763f6
Axizeaus <axizeaus@gmail.com>
2023-04-18 16:07:57 +0630

    finished 2.6, 2.7, 2.8

---
+++ phonebook/src/App.js
@@ -0,0 +1,96 @@
+import { useState } from "react";
+
+const App = () => {
+  
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState('');
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (personCheck(newName)){
+      if(newName === '' || newNumber === ''){
+        alert("fill the form properly");
+      } else {
+        setPersons(persons.concat({ name: newName, number: newNumber }));
+      }
+      
+    } else {
+      alert(`${newName} is already on the list`)
+    }
+    
+  }
+
+  const personCheck = (newPerson) => {
+    for (let person of persons){
+      if (person.name.toLowerCase() === newPerson.toLowerCase()){
+        return false
+      }
+    }
+    return true
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <PersonForm 
+        onSubmit={addPerson}
+        name = {newName}
+        number = {newNumber}
+        onNameChange={handleNameChange}
+        onNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <ul>
+        {
+          persons.map((person) => 
+          <Person name={person.name} key={person.name} number={person.number}/>)
+        }
+      </ul>
+    </div>
+  );
+};
+
+const PersonForm = ({onSubmit, name, number, onNameChange, onNumberChange}) => {
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <div>
+          name:
+          <input value={name} onChange={onNameChange} />
+        </div>
+        <div>
+          number:
+          <input value={number} onChange={onNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+const Person = ({name, number}) => {
+  return (
+    <>
+        <li>{name}</li>
+        <li>{number}</li>
+    </>
+  );
+}
+
+export default App;
