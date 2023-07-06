4d01876cca397797258a9b53ea2d6501556a832b
Axizeaus <axizeaus@gmail.com>
2023-07-06 09:40:49 +0630

    update button assigned to its relative id

--- phonebook/src/components/Display.js
+++ phonebook/src/components/Display.js
@@ -4,7 +4,7 @@ export default function Display({display, onUpdate}) {
   return (
     <>
       <ul>
-        {display.map(person => <Person name={person.name} key={person.id} number={person.number} onUpdate={onUpdate} />
+        {display.map(person => <Person name={person.name} key={person.id} number={person.number} onUpdate={() => onUpdate(person.id)} />
         )}
       </ul>
     </>
