faf5367424771cc8a32da386a19105eb1f1e5b97
Axizeaus <axizeaus@gmail.com>
2023-07-21 20:17:13 +0630

    made build and copied it to the backend dir

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,127 +1,131 @@
 import { useState, useEffect } from "react";
 import PersonForm from "./components/PersonForm";
 import Display from "./components/Display";
-import personService from './services/personService';
+import personService from "./services/personService";
 import Message from "./components/Message";
 
 const App = () => {
-  
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState('');
-  const [filter, setFilter] = useState('');
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
   const [display, setDisplay] = useState([]);
-  const [message, setMessage] = useState('is this even working');
-  
+  const [message, setMessage] = useState("");
+
   const hook = () => {
     personService.getAll().then((response) => {
       setPersons(response);
       setDisplay(response);
       setMessage(null);
     });
-  }
-  
+  };
+
   useEffect(hook, []);
-  console.log('display => ', display)
+  console.log("display => ", display);
 
   function messageShowAndDelete(message) {
-    setMessage(message)
+    setMessage(message);
     setTimeout(() => {
       setMessage(null);
     }, 5000);
   }
   const addPerson = (event) => {
-    event.preventDefault()
-    if (personCheck(newName)){
-      if(newName === '' || newNumber === ''){
+    event.preventDefault();
+    if (personCheck(newName)) {
+      if (newName === "" || newNumber === "") {
         messageShowAndDelete("fill the form properly");
-        
       } else {
         const personObject = {
-          name : newName,
-          number : newNumber
-        }
+          name: newName,
+          number: newNumber,
+        };
         console.log("add person runs");
-        personService.create(personObject)
-          .then((response) => {
-            setPersons(persons.concat(response))
-            setDisplay(display.concat(response))
-            messageShowAndDelete(`Added ${personObject.name} successfully`);
-          });
+        personService.create(personObject).then((response) => {
+          setPersons(persons.concat(response));
+          setDisplay(display.concat(response));
+          messageShowAndDelete(`Added ${personObject.name} successfully`);
+        });
       }
     }
-  }
+  };
 
   const personCheck = (newPerson) => {
-    for (let person of persons){
-      if (person.name.toLowerCase() === newPerson.toLowerCase()){
-        return false
+    for (let person of persons) {
+      if (person.name.toLowerCase() === newPerson.toLowerCase()) {
+        return false;
       }
     }
-    return true
-  }
+    return true;
+  };
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
   const handleFilterChange = (event) => {
-    const filterData = event.target.value
-    setFilter(filterData)
+    const filterData = event.target.value;
+    setFilter(filterData);
     const re = new RegExp(filterData.toLowerCase());
-    let found = []
-    persons.forEach((person)=>{
+    let found = [];
+    persons.forEach((person) => {
       if (re.test(person.name.toLowerCase())) {
-        found.push(person)
-      };
-    })
+        found.push(person);
+      }
+    });
     setDisplay(found);
-    messageShowAndDelete('filtered list')
-  }
+    messageShowAndDelete("filtered list");
+  };
 
   const handleUpdateChange = (id) => {
-    const target = persons.find(person => person.id === id);
+    const target = persons.find((person) => person.id === id);
     const updatedPerson = {
-      name: window.prompt('Enter name or click ok', target.name),
-      number: window.prompt('enter new number or click ok', target.number)
-    }
-    personService.update(target.id, updatedPerson)
+      name: window.prompt("Enter name or click ok", target.name),
+      number: window.prompt("enter new number or click ok", target.number),
+    };
+    personService
+      .update(target.id, updatedPerson)
       .then(() => {
         messageShowAndDelete("Updated successfully");
-         hook()
-        })
-      .catch(error => {
-        messageShowAndDelete('Updating failed');
+        hook();
       })
-  }
+      .catch((error) => {
+        messageShowAndDelete("Updating failed");
+      });
+  };
 
   const handleDelete = (id) => {
-    const target = persons.find(person => person.id === id);
-    if (target && window.confirm('do you really wanna delete this contact?')){
-      personService.del(id).then(() => messageShowAndDelete('Deletion successful.'));
+    const target = persons.find((person) => person.id === id);
+    if (target && window.confirm("do you really wanna delete this contact?")) {
+      personService
+        .del(id)
+        .then(() => messageShowAndDelete("Deletion successful."));
     }
-    setDisplay(display.filter(person => person.id !== id))
-  }
+    setDisplay(display.filter((person) => person.id !== id));
+  };
 
   return (
     <>
       <h1>Phonebook</h1>
-      <Message message={message}/>
-      <PersonForm 
+      {message && <Message message={message} />}
+      <PersonForm
         onSubmit={addPerson}
-        name = {newName}
-        number = {newNumber}
+        name={newName}
+        number={newNumber}
         onNameChange={handleNameChange}
         onNumberChange={handleNumberChange}
       />
       <h2>Filter</h2>
-      <input value={filter} onChange={handleFilterChange}/>
+      <input value={filter} onChange={handleFilterChange} />
       <h2>Numbers</h2>
-      <Display display={display} onDelete={handleDelete} onUpdate={handleUpdateChange}/>
+      <Display
+        display={display}
+        onDelete={handleDelete}
+        onUpdate={handleUpdateChange}
+      />
     </>
   );
 };
